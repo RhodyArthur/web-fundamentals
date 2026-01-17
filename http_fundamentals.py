acc64417c478f2383b827e5d29ab913d620772b2
RhodyArthur <rhodaarthur48@gmail.com>
2026-01-17 19:30:50 +0000

    feat: add functions to demonstrate HTTP methods

---
+++ http_fundamentals.py
@@ -0,0 +1,39 @@
+# Task 1.1: Understanding HTTP Methods (5 points)
+# Write functions that demonstrate different HTTP methods:
+
+import requests
+BASE_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+def demonstrate_get(url):
+    """
+    Make a GET request to the URL
+    Print: status code, headers (Content-Type), and response body
+    Return: response object
+    """
+    r = requests.get(url)
+    print(f"Status Code: {r.status_code}\n Headrs (Content-Type): {r.headers} \n Response body: {r.text}")
+    return r
+
+def demonstrate_post(url, data):
+    """
+    Make a POST request with JSON data
+    Print: status code and response body
+    Return: response object
+    """
+    pass
+
+def demonstrate_put(url, data):
+    """
+    Make a PUT request to update a resource
+    Print: status code and response body
+    Return: response object
+    """
+    pass
+
+def demonstrate_delete(url):
+    """
+    Make a DELETE request
+    Print: status code
+    Return: True if successful (status 200-299), False otherwise
+    """
+    pass
